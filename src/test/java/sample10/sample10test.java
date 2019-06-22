b793c3454aacc3ad737935b7004601ca57fadded
mounika879 <48467095+mounika879@users.noreply.github.com>
2019-06-22 16:57:40 +0530

    Add files via upload

---
+++ src/test/java/sample10/sample10test.java
@@ -0,0 +1,14 @@
+package sample10;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sample10test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
